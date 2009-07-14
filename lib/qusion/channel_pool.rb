3b07cb0b5c261fd3b5df555858b0e21c97a72ac0
Dan DeLeo <danielsdeleo@mac.com>
2009-07-13 18:34:42 -0600

    Rdoc for channel pools

--- lib/qusion/channel_pool.rb
+++ lib/qusion/channel_pool.rb
@@ -2,6 +2,23 @@
 require "singleton"
 
 module Qusion
+  
+  # ChannelPool maintains a pool of AMQP channel objects that can be reused.
+  # The motivation behind this is that if you were to use MQ.new to create a 
+  # new channel for every request, your AMQP broker could be swamped trying to
+  # maintain a bunch of channels that you're only using once.
+  #
+  # To use the channel pool, just replace <tt>MQ.new</tt> in your code with <tt>Qusion.channel</tt>
+  # 
+  #   # Instead of this:
+  #   MQ.new.queue("my-worker-queue")
+  #   # Do this:
+  #   Qusion.channel.queue("my-worker-queue")
+  #
+  # By default, ChannelPool maintains a pool of 5 channels. This can be adjusted with
+  # <tt>ChannelPool.pool_size=()</tt> or <tt>Qusion.channel_pool_size()</tt>
+  # The optimal pool size is not yet known, but I suspect you might need a 
+  # larger value if using Thin in production, and a smaller value otherwise.
   class ChannelPool
     include Singleton
     
