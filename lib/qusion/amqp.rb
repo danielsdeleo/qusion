ce8a634776b2fffb80e2fbaec13fc16254d04000
Dan DeLeo <danielsdeleo@mac.com>
2009-08-03 22:37:34 -0600

    reorder app server checks, seems to play nicer with rails on rack * this doesn't seem so sustainable though... would be nice to have a way to ask rack what server (handler) is being used

--- lib/qusion/amqp.rb
+++ lib/qusion/amqp.rb
@@ -15,14 +15,14 @@ module AMQP
         end
         die_gracefully_on_signal
       end
-    elsif defined?(::Thin)
-      @settings = settings.merge(*args)
-      die_gracefully_on_signal
     elsif defined?(::Mongrel)
       th = Thread.current 
       Thread.new do
         self.start(*args) 
       end
+      die_gracefully_on_signal
+    elsif defined?(::Thin)
+      @settings = settings.merge(*args)
       die_gracefully_on_signal
     else
       puts "=> Qusion did not find a supported app server (This is normal for workling workers)"
