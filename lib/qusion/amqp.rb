8f2853874771e32aceb3bdd862cca0ca83d3b2ce
Dan DeLeo <danielsdeleo@mac.com>
2009-08-03 18:54:34 -0600

    Explicitly test for ::Mongrel--fixes Workling clients ! This breaks webrick support (if there ever was any, never tried)

--- lib/qusion/amqp.rb
+++ lib/qusion/amqp.rb
@@ -18,12 +18,14 @@ module AMQP
     elsif defined?(::Thin)
       @settings = settings.merge(*args)
       die_gracefully_on_signal
-    else
+    elsif defined?(::Mongrel)
       th = Thread.current 
       Thread.new do
         self.start(*args) 
       end
       die_gracefully_on_signal
+    else
+      puts "=> Qusion did not find a supported app server (This is normal for workling workers)"
     end
   end
   
